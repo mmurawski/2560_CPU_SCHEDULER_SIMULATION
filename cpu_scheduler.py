003af8d649cc0a431a7d2937ab7d6d5723acc7cf
Etnad13 <danjiji13@gmail.com>
2020-05-05 12:25:36 -0700

    Added 2 styles
    First Come first serve added
    Round Robin still needs a fixin on line 98-99

--- cpu_scheduler.py
+++ cpu_scheduler.py
@@ -55,3 +55,81 @@ class cpu:
     #need to implement overloading. 
     # cpu cores need to be comparable based on their bursts required to be free again.
         
+        
+"""
+    Order that job arrives indicates its importance level.
+    No matter the length of the job, whatever job comes first
+    will be served and completed first.
+"""
+def FCFS(Jobs, Jobsize):
+    totalJobLength = 0 # initialized totaljoblength as zero
+    ATAT = 0
+    TimeSum = 0
+    JobEndTime  = []
+    
+    for i in range(len(Jobs)):
+        totalJobLength = totalJobLength + Jobsize[i]
+        JobEndTime.insert(len(JobEndTime),totalJobLength)
+        
+    print("\n Job Name \t | \t Job Endtime \n")
+    print("------------------------------------")
+    for j in range(len(Jobs)):
+        print(Jobs[j], "\t | \t",JobEndTime[j],"ms \n")
+        TimeSum = TimeSum + JobEndTime[j]
+        
+    ATAT = TimeSum / len(Jobs)
+    print("\n Average Turnaround Time:",ATAT,"ms \n")
+    return
+
+def RoundRobin(Jobs,Jobsize,Slice):
+    FinalJob = []
+    tempEndTime = []
+    finalJobTime = []
+    totalJobLength = 0
+    newSize = 0
+    
+    while len(Jobsize)!=0:
+        for i in range(len(Jobsize)):
+            if Jobsize[i] <= Slice:
+                newSize = newSize + Jobsize[i]
+                tempEndTime.insert(len(tempEndTime),newSize)
+                finalJobTime.insert(len(finalJobTime),newSize)
+                FinalJob.insert(len(FinalJob),Jobs[i])
+               # Jobsize.erase(Jobsize.begin() + i)  # Since the job is completed, we can erase the current job length from circulation.
+				#Jobs.erase(Jobs.begin() + i)	# The corresponding job name is also erased from circulation.
+                totalJobLength = totalJobLength + newSize
+                i = i - 1
+            else:
+                newSize = newSize + Slice
+                Jobsize[i] = Jobsize[i] - Slice
+                tempEndTime.insert(len(tempEndTime),newSize)
+    
+    print("| \n Job Name \t | \t Job Endtime \n")
+    print("------------------------------------")
+    for j in range(len(FinalJob)):
+        print(FinalJob[i],"\t | \t", finalJobTime[i], "ms \n")
+    print("\n Average Turnaround Time: ",(totalJobLength/len(FinalJob)),"ms \n")
+    
+    return
+    
+
+def main():
+    #read file maybe
+    Jobs = ["Job1","Job2","Job3","Job4","Job5"]
+    Jobsize = [7,18,10,4,12]
+    print("---------------------------")
+    print("First come, First Served")
+    FCFS(Jobs, Jobsize)
+    
+    print("---------------------------")
+    print("Round Robin with time slice 2")
+    RoundRobin(Jobs, Jobsize,2)
+    
+    
+    return
+
+main() 
+        
+        
+        
+    
